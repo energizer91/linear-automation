3744f68f213dc51218ee3effce957e316de41594
Alexander Bareyko <militaryboy@yandex.ru>
2023-12-12 20:17:08 +0100

    run prettier and optimize code

--- handler.js
+++ handler.js
@@ -1,25 +1,25 @@
-const { LinearClient } = require('@linear/sdk');
+const { LinearClient } = require("@linear/sdk");
 
 const LABELS = {
-	EXPERIMENT: 'f8adea6b-1abc-42c3-b1ec-4d709f3141af',
-	AUTOMATED: '867440f5-9170-4d1f-91be-dd88220efe03',
-	HOLD: 'f892a5ef-06e4-4294-b718-69b93de685c4',
-	DEV: '70e005e1-136c-4bcc-a7b4-605a374c141d',
-	DESIGN: '3cf48a10-e71c-4fff-8b27-f6c732039600',
+  EXPERIMENT: "f8adea6b-1abc-42c3-b1ec-4d709f3141af",
+  AUTOMATED: "867440f5-9170-4d1f-91be-dd88220efe03",
+  HOLD: "f892a5ef-06e4-4294-b718-69b93de685c4",
+  DEV: "70e005e1-136c-4bcc-a7b4-605a374c141d",
+  DESIGN: "3cf48a10-e71c-4fff-8b27-f6c732039600",
 };
 
 const STATUSES = {
-	BACKLOG: '1b3761ae-75a4-436b-8c47-9db13fa0c429',
-	REFINEMENT: '794c06b9-eb3c-4292-bc4c-bcdcc8916674',
-	COMING_UP: '6ce97593-a310-4ff1-988c-761045a9a9d4',
+  BACKLOG: "1b3761ae-75a4-436b-8c47-9db13fa0c429",
+  REFINEMENT: "794c06b9-eb3c-4292-bc4c-bcdcc8916674",
+  COMING_UP: "6ce97593-a310-4ff1-988c-761045a9a9d4",
 };
 
 const TEMPLATES = {
-	WEBSITE: 'ff0baae3-b3fc-4a27-bfa9-87125cf0d2ba',
-	CLEANUP: '434cf2fe-e0b6-4c22-8f3b-0ea3c10ec633',
-	DEV: '3a99effc-685c-4379-8af7-c1e9a300beb2',
-	DESIGN: '4de697d1-5926-43fa-a4ad-d32e942afd31',
-	EXPERIMENT: '9cd13017-3a91-44e8-a852-23822a8c9a68',
+  WEBSITE: "ff0baae3-b3fc-4a27-bfa9-87125cf0d2ba",
+  CLEANUP: "434cf2fe-e0b6-4c22-8f3b-0ea3c10ec633",
+  DEV: "3a99effc-685c-4379-8af7-c1e9a300beb2",
+  DESIGN: "4de697d1-5926-43fa-a4ad-d32e942afd31",
+  EXPERIMENT: "9cd13017-3a91-44e8-a852-23822a8c9a68",
 };
 
 const experimentDescription = (description) => `${description}
@@ -47,94 +47,96 @@ const designDescription = (description) => `${description}
 
 const linearClient = new LinearClient({ apiKey: process.env.LINEAR_API_KEY });
 
+const formatResponse = (body, statusCode = 200) => ({
+  statusCode,
+  body: JSON.stringify(body),
+});
+
 const handler = async (event) => {
-	if (!event.body) {
-		return {
-			statusCode: 200,
-			body: JSON.stringify({ message: 'No webhook data' }),
-		};
-	}
+  if (!event.body) {
+    return formatResponse({ message: "No webhook data" });
+  }
 
-	try {
-		const payload = JSON.parse(event.body);
-		const { data: issue, updatedFrom = {} } = payload;
-		const { description } = issue;
+  try {
+    const payload = JSON.parse(event.body);
+    const { data: issue, updatedFrom = {} } = payload;
+    const { description } = issue;
 
-		const isExperiment = issue.labelIds.includes(LABELS.EXPERIMENT);
-		const isAutomated = issue.labelIds.includes(LABELS.AUTOMATED);
-		const isMovedToRefinement = issue.stateId === STATUSES.REFINEMENT;
-		const isMovedFromBacklog = updatedFrom.stateId === STATUSES.BACKLOG;
+    const isExperiment = issue.labelIds.includes(LABELS.EXPERIMENT);
+    const isAutomated = issue.labelIds.includes(LABELS.AUTOMATED);
+    const isMovedToRefinement = issue.stateId === STATUSES.REFINEMENT;
+    const isMovedFromBacklog = updatedFrom.stateId === STATUSES.BACKLOG;
 
-		if (isAutomated || !isExperiment || !isMovedToRefinement || !isMovedFromBacklog) {
-			// Invalid case, early return
+    if (
+      isAutomated ||
+      !isExperiment ||
+      !isMovedToRefinement ||
+      !isMovedFromBacklog
+    ) {
+      // Invalid case, early return
 
-			return {
-				statusCode: 200,
-				body: JSON.stringify({
-					message: 'Webhook issue status invalid',
-					isExperiment,
-					isAutomated,
-					isMovedToRefinement,
-					isMovedFromBacklog,
-				}),
-			};
-		}
+      return formatResponse({
+        message: "Webhook issue status invalid",
+        isExperiment,
+        isAutomated,
+        isMovedToRefinement,
+        isMovedFromBacklog,
+      });
+    }
 
-		// Update experiment issue
-		await linearClient.updateIssue(issue.id, {
-			description: experimentDescription(description),
-			labelIds: issue.labelIds.concat(LABELS.AUTOMATED),
-		});
+    // Update experiment issue
+    await linearClient.updateIssue(issue.id, {
+      description: experimentDescription(description),
+      labelIds: issue.labelIds.concat(LABELS.AUTOMATED),
+    });
 
-		// Create dev task
-		await linearClient.createIssue({
-			teamId: issue.teamId,
-			cycleId: issue.cycleId,
-			parentId: issue.id,
-			title: `[Dev] ${issue.title}`,
-			stateId: STATUSES.COMING_UP,
-			labelIds: [LABELS.DEV, LABELS.HOLD],
-			templateId: TEMPLATES.DEV,
-			description: devDescription(description),
-			priority: issue.priority,
-		});
+    // Create dev task
+    await linearClient.createIssue({
+      teamId: issue.teamId,
+      cycleId: issue.cycleId,
+      parentId: issue.id,
+      title: `[Dev] ${issue.title}`,
+      stateId: STATUSES.COMING_UP,
+      labelIds: [LABELS.DEV, LABELS.HOLD],
+      templateId: TEMPLATES.DEV,
+      description: devDescription(description),
+      priority: issue.priority,
+    });
 
-		// Create design task
-		await linearClient.createIssue({
-			teamId: issue.teamId,
-			cycleId: issue.cycleId,
-			parentId: issue.id,
-			title: `[Design] ${issue.title}`,
-			stateId: issue.stateId,
-			labelIds: [LABELS.DESIGN],
-			templateId: TEMPLATES.DESIGN,
-			description: designDescription(description),
-			priority: issue.priority,
-		});
+    // Create design task
+    await linearClient.createIssue({
+      teamId: issue.teamId,
+      cycleId: issue.cycleId,
+      parentId: issue.id,
+      title: `[Design] ${issue.title}`,
+      stateId: issue.stateId,
+      labelIds: [LABELS.DESIGN],
+      templateId: TEMPLATES.DESIGN,
+      description: designDescription(description),
+      priority: issue.priority,
+    });
 
-		// Create cleanup task
-		await linearClient.createIssue({
-			teamId: issue.teamId,
-			cycleId: issue.cycleId,
-			parentId: issue.id,
-			title: `[Clean up] ${issue.title}`,
-			stateId: STATUSES.COMING_UP,
-			labelIds: [LABELS.DEV, LABELS.HOLD],
-			templateId: TEMPLATES.DEV,
-			priority: issue.priority,
-		});
+    // Create cleanup task
+    await linearClient.createIssue({
+      teamId: issue.teamId,
+      cycleId: issue.cycleId,
+      parentId: issue.id,
+      title: `[Clean up] ${issue.title}`,
+      stateId: STATUSES.COMING_UP,
+      labelIds: [LABELS.DEV, LABELS.HOLD],
+      templateId: TEMPLATES.DEV,
+      priority: issue.priority,
+    });
 
-		// Return a response
-		return {
-			statusCode: 200,
-			body: JSON.stringify({ message: 'Webhook processed' }),
-		};
-	} catch (e) {
-		return {
-			statusCode: 500,
-			body: JSON.stringify({ message: 'Internal Server Error', error: e.message, stack: e.stack }),
-		};
-	}
+    // Return a response
+    return formatResponse({ message: "Webhook processed" });
+  } catch (e) {
+    return formatResponse({
+      message: "Internal Server Error",
+      error: e.message,
+      stack: e.stack,
+    });
+  }
 };
 
 module.exports = handler;
--- server.js
+++ server.js
@@ -1,40 +1,40 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const http = require('http');
-const handler = require('./handler');
+const http = require("http");
+const handler = require("./handler");
 
 const server = http.createServer(async (req, res) => {
-    if (req.method !== 'POST') {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ ok: true }));
-        return;
+  if (req.method !== "POST") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ ok: true }));
+    return;
+  }
+
+  // Collect data chunks
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk.toString(); // Convert Buffer to string
+  });
+
+  // Once all data is received
+  req.on("end", async () => {
+    // console.log(body); // Log the complete body
+    console.log("new webhook event");
+
+    try {
+      const result = await handler({ body });
+
+      console.log("handler successfully finished with result", result);
+
+      // Send a response back to the client
+      res.writeHead(result.statusCode, { "Content-Type": "application/json" });
+      res.end(result.body);
+    } catch (e) {
+      console.error("Webhook error", e);
+      res.writeHead(500, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ e }));
     }
-
-    // Collect data chunks
-    let body = '';
-    req.on('data', (chunk) => {
-        body += chunk.toString(); // Convert Buffer to string
-    });
-
-    // Once all data is received
-    req.on('end', async () => {
-        // console.log(body); // Log the complete body
-        console.log('new webhook event');
-
-        try {
-            const result = await handler({ body });
-
-            console.log('handler successfully finished with result', result);
-
-            // Send a response back to the client
-            res.writeHead(result.statusCode, { 'Content-Type': 'application/json' });
-            res.end(result.body);
-        } catch (e) {
-            console.error('Webhook error', e);
-            res.writeHead(500, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({e}));
-        }
-    });
+  });
 });
 
 const PORT = process.env.LISTEN_PORT || 3000;
